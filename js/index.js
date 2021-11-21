9174f8c09091e5ea8f6433780510c94570d53046
Евгений Евгений <savok310586@gmail.com>
2021-11-21 18:09:36 +0200

    made in switch...case

--- js/index.js
+++ js/index.js
@@ -3,20 +3,36 @@
 const MAX_PLASES = 50;
 let firstPlace,
     secondPlace;
-let answer = +prompt('Введите номер места');
+let answer = +prompt('Введите номер места')
+firstPlace = answer;
+secondPlace = firstPlace + 1;
 
-if (answer < 1 || answer > MAX_PLASES|| isNaN(answer)) {
+/*if (answer < 1 || answer > MAX_PLASES|| isNaN(answer)) {
     alert('Не правильный номер места! Введите номер от 1 до 50.')
 } else if (!(answer % 3)) {
     alert('Место забронированно в связи с карантином')
 } else if (answer < 20 || answer > 30) {
     alert('Место забронированно')
 } else if (answer > 20 || answer < 30) {
-    firstPlace = answer;
-    secondPlace = firstPlace + 1;
     alert(`Место свободно. Ближайшее место ${secondPlace}`)
-}
-
-/*switch(firstPlace) {
-    case ''
+    ;
 }*/
+
+switch(true) {
+    case answer < 1:
+    case answer > MAX_PLASES:
+    case isNaN(answer):
+        alert('Не правильный номер места! Введите номер от 1 до 50.')
+        break
+    case !(answer % 3):
+        alert('Место забронированно в связи с карантином')
+        break
+    case answer < 20:
+    case answer > 30:
+        alert('Место забронированно')
+        break
+    case answer > 20:
+    case answer < 30:
+        alert(`Место свободно. Ближайшее место ${secondPlace}`)  
+        break
+}
